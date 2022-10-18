9899a5434ae98967e158156b2f79d0335836dd67
TasawarSaraf <tazsaraf@gmail.com>
2022-10-18 00:06:22 -0400

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Tasawar Saraf on 10/18/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
